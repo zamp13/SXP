eca2a6313f434fafa7fd92dc161eb28041ec6775
Thomas <delfino.thomas@gmail.com>
2017-11-15 10:18:07 +0100

    Correction HashTest.java, maitenant tout les tests passent

--- src/test/java/protocol/impl/sigma/HashTest.java
+++ src/test/java/protocol/impl/sigma/HashTest.java
@@ -4,58 +4,92 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
 public class HashTest {
 
-    public String password1, password2, password2bis;
-    public byte[] salt1, salt2, salt2bis;
-    public byte[] hash1, hash2, hash2bis;
+    public static String password1, password2, password2bis;
+    public static byte[] salt1, salt2, salt2bis;
+    public static byte[] hash1, hash2, hash2bis;
 
     @BeforeClass
-    public static void initialize(){
-        password1 = "TotallySecurePassword";
-        password2 = "TotallySecurePassword12345";
-        password2bis = "TotallySecurePassword12345";
-        salt1 = Hash.generateSalt();
-        salt2 = Hash.generateSalt();
-        salt2bis = Hash.generateSalt();
-        hash1 = Hash.calculateHash(password.getBytes("UTF-8"), salt1);
-        hash2 = Hash.calculateHash(password2.getBytes("UTF-8"), salt2);
-        hash2bis = Hash.calculateHash(password2bis.getBytes("UTF-8"), salt2bis);
+    public static void initialize()
+    {
+        try
+        {
+			password1 = "TotallySecurePassword";
+			password2 = "TotallySecurePassword12345";
+			password2bis = "TotallySecurePassword12345";
+			salt1 = Hash.generateSalt();
+			salt2 = Hash.generateSalt();
+			salt2bis = Hash.generateSalt();
+			hash1 = Hash.calculateHash(password1.getBytes("UTF-8"), salt1);
+			hash2 = Hash.calculateHash(password2.getBytes("UTF-8"), salt2);
+			hash2bis = Hash.calculateHash(password2bis.getBytes("UTF-8"), salt2bis);
+		}catch(Exception e)
+		{
+			fail("Exception");
+		}
     }
 
     @Test
-    public void hashage(){
+    public void testPassword1(){
         // password1
-        assertTrue( Hash.verifyPassword(hash1, password1.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash2, password1.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash2bis, password1.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash1, password1.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash2, password1.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash2bis, password1.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash1, password1.getBytes("UTF-8"), salt2bis) );
-        assertFalse( Hash.verifyPassword(hash2, password1.getBytes("UTF-8"), salt2bis) );
-        assertFalse( Hash.verifyPassword(hash2bis, password1.getBytes("UTF-8"), salt2bis) );
-        //password2
-        assertTrue( Hash.verifyPassword(hash1, password2.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash2, password2.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash2bis, password2.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash1, password2.getBytes("UTF-8"), salt2) );
-        assertTrue( Hash.verifyPassword(hash2, password2.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash2bis, password2.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash1, password2.getBytes("UTF-8"), salt2bis) );
-        assertFalse( Hash.verifyPassword(hash2, password2.getBytes("UTF-8"), salt2bis) );
-        assertFalse( Hash.verifyPassword(hash2bis, password2.getBytes("UTF-8"), salt2bis) );
-        //password2bis
-        assertTrue( Hash.verifyPassword(hash1, password2bis.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash2, password2bis.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash2bis, password2bis.getBytes("UTF-8"), salt1) );
-        assertFalse( Hash.verifyPassword(hash1, password2bis.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash2, password2bis.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash2bis, password2bis.getBytes("UTF-8"), salt2) );
-        assertFalse( Hash.verifyPassword(hash1, password2bis.getBytes("UTF-8"), salt2bis) );
-        assertFalse( Hash.verifyPassword(hash2, password2bis.getBytes("UTF-8"), salt2bis) );
-        assertTrue( Hash.verifyPassword(hash2bis, password2bis.getBytes("UTF-8"), salt2bis) );
-    }
-
-
+        try
+        {
+			assertTrue( Hash.verifyPassword(hash1, password1.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash2, password1.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash2bis, password1.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash1, password1.getBytes("UTF-8"), salt2) );
+			assertFalse( Hash.verifyPassword(hash2, password1.getBytes("UTF-8"), salt2) );
+			assertFalse( Hash.verifyPassword(hash2bis, password1.getBytes("UTF-8"), salt2) );
+			assertFalse( Hash.verifyPassword(hash1, password1.getBytes("UTF-8"), salt2bis) );
+			assertFalse( Hash.verifyPassword(hash2, password1.getBytes("UTF-8"), salt2bis) );
+			assertFalse( Hash.verifyPassword(hash2bis, password1.getBytes("UTF-8"), salt2bis) );
+		}
+		catch (Exception e)
+		{
+			fail("Exception");
+		}
+	}
+	@Test
+	public void testPassword2(){
+        // password2
+        try
+        {
+			assertFalse( Hash.verifyPassword(hash1, password2.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash2, password2.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash2bis, password2.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash1, password2.getBytes("UTF-8"), salt2) );
+			assertTrue( Hash.verifyPassword(hash2, password2.getBytes("UTF-8"), salt2) ); //true car password2bis = password2
+			assertFalse( Hash.verifyPassword(hash2bis, password2.getBytes("UTF-8"), salt2) );
+			assertFalse( Hash.verifyPassword(hash1, password2.getBytes("UTF-8"), salt2bis) );
+			assertFalse( Hash.verifyPassword(hash2, password2.getBytes("UTF-8"), salt2bis) );
+			assertTrue( Hash.verifyPassword(hash2bis, password2.getBytes("UTF-8"), salt2bis) );
+		}
+		catch (Exception e)
+		{
+			fail("Exception");
+		}
+	}
+	@Test
+	public void testPassword2bis(){
+        // password2bis
+        try
+        {
+			//password2bis
+			assertFalse( Hash.verifyPassword(hash1, password2bis.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash2, password2bis.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash2bis, password2bis.getBytes("UTF-8"), salt1) );
+			assertFalse( Hash.verifyPassword(hash1, password2bis.getBytes("UTF-8"), salt2) );
+			assertTrue( Hash.verifyPassword(hash2, password2bis.getBytes("UTF-8"), salt2) ); //true car password2bis = password2
+			assertFalse( Hash.verifyPassword(hash2bis, password2bis.getBytes("UTF-8"), salt2) );
+			assertFalse( Hash.verifyPassword(hash1, password2bis.getBytes("UTF-8"), salt2bis) );
+			assertFalse( Hash.verifyPassword(hash2, password2bis.getBytes("UTF-8"), salt2bis) );
+			assertTrue( Hash.verifyPassword(hash2bis, password2bis.getBytes("UTF-8"), salt2bis) );
+		}
+		catch (Exception e)
+		{
+			fail("Exception");
+		}
+	}
 }
