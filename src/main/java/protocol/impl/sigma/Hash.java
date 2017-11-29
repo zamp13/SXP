fb7f2bbbe6dabf20f2c23dd1679b1b88bb876ee8
Thomas <delfino.thomas@gmail.com>
2017-11-29 11:00:50 +0100

    Commentaire classe de hash

--- src/main/java/protocol/impl/sigma/Hash.java
+++ src/main/java/protocol/impl/sigma/Hash.java
@@ -1,26 +1,5 @@
 /**
- * Exemple
-public static void main(String[] args)
-{ 
-	String password = "TotallySecurePassword12345"; 
-	String password2 = "TotallySecurePassword"; 
-	try 
-	{ 
-		byte[] salt = Hash.generateSalt(); 
-		System.out.print("Salt: " + byteArrayToHexString(salt)+"\n");
-		System.out.print("Password bytes: "+ byteArrayToHexString(password.getBytes("UTF-8")) +"\n");
-		byte[] hash = Hash.calculateHash(password.getBytes("UTF-8"), salt); 
-		System.out.print("Hash: " + byteArrayToHexString(hash)+"\n");
-		boolean correct = Hash.verifyPassword(hash, password2.getBytes("UTF-8"), salt); 
-		if(correct)
-			System.out.print("Identique\n");
-		else
-			System.out.print("Different\n");
-	}
-	catch (NoSuchAlgorithmException | UnsupportedEncodingException ex)
-	{ 
-		System.out.print(ex.getMessage()); 
-	}
+ *	Class for hashing a password
 } **/
  
 package protocol.impl.sigma;
@@ -30,14 +9,14 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom; 
 public abstract class Hash 
 { 
-    private static final String ALGORITHM = "SHA-512"; //Choix de l'algorithme de cryptage
-    private static final int ITERATIONS = 100000;  //Nombre d'itération pour le calcul du hash
-    private static final int SALT_SIZE = 64; //Taille du sel
+    private static final String ALGORITHM = "SHA-512"; //Choosing the encryption algorithm
+    private static final int ITERATIONS = 100000;  //Iteration number for hash calculation
+    private static final int SALT_SIZE = 64; //Size of salt
     
 	/**
-	 * Fonction utilise pour le débug elle convertie un tableau de byte en hexadécimal
-	 * @param tableau de bit
-	 * @return un string hexadécimal **/
+	 * Function uses for debug, it converts a byte array to hexadecimal
+	 * @param	byte array to convert
+	 * @return 	 hexadecimal string **/
     public static String byteArrayToHexString(byte[] bArray)
 	{
 		StringBuffer buffer = new StringBuffer();
@@ -49,9 +28,9 @@ public abstract class Hash
 		return buffer.toString().toUpperCase();
     }
 	/**
-	 * Fonction qui génére un sel 
-	 * @param aucun 
-	 * @return un tableau de byte **/
+	 * Function which generates the salt
+	 * @param	
+	 * @return  random salt **/
     public static byte[] generateSalt() 
     { 
         SecureRandom random = new SecureRandom(); 
@@ -62,12 +41,12 @@ public abstract class Hash
     } 
 	
 	/**
-	 * Fonction qui calcule un hash
-	 * @param passwordByte: un tableau de byte qui represente le nombre
-	 * @param salt un tableau de byte qui represente le salt
-	 * @throws NoSuchAlgorithmException si il ne trouve pas l'algorithme de hashage
-	 * @throws UnsupportedEncodingException si il ne supporte pas l'encodage
-	 * @return le hash calculé*/
+	 * Fonction which calculates the hash
+	 * @param passwordByte it takes the Password
+	 * @param salt	it takes the salt
+	 * @throws NoSuchAlgorithmException  if it didn't find the algorithme of hash 
+	 * 	 * @throws UnsupportedEncodingException if it didn't support encoding
+	 * @return A hash */
     public static byte[] calculateHash(byte[] passwordByte, byte[] salt) throws NoSuchAlgorithmException, UnsupportedEncodingException 
     { 
         byte[] concat = new byte[salt.length + passwordByte.length];
@@ -87,11 +66,11 @@ public abstract class Hash
     } 
     
     /**
-     * Fonction qui vérifie un password
-     * @param originalHash	 le hash original
-     * @param password	 le mots de passe à vérifier
-     * @param salt	 le salt
-     * @return un booleen qui indique si le mots de passe est identique(true) ou différent(false)*/
+     * Compare pasword and password+salt 
+     * @param originalHash	 Orignal hash
+     * @param password	 The comparison password hash
+     * @param salt	The comparison password salt
+     * @return True if both match, false otherwise */
     public static boolean verifyPassword(byte[] originalHash, byte[] password, byte[] salt) throws NoSuchAlgorithmException, UnsupportedEncodingException
     { 
         byte[] comparisonHash = calculateHash(password, salt); 
